08d152b94099bf18b1449c0e315e5aed54a11d26
JadaGB <jadabaileye@gmail.com>
2023-01-15 23:20:29 -0500

    Initial commit

---
+++ screens/Details.js
@@ -0,0 +1,11 @@
+//basic arrow function component shortcut - rafce (reactArrowFunnctionExportComponent)
+import React from 'react'
+import { Text } from 'react-native'
+
+const Details = () => {
+  return (
+    <Text>Details</Text>
+  )
+}
+
+export default Details
